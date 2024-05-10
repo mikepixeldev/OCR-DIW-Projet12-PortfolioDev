b9b3ae3e6a8c2b89376558ff57d9473c55f8e38b
Mike Pixel Dev <86058062+mikepixeldev@users.noreply.github.com>
2024-05-10 04:02:03 +0200

    Ajout bouton de retour en haut

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
+// Script pour afficher l'année en cours dans le footer
 document.addEventListener("DOMContentLoaded", function () {
   var currentYear = new Date().getFullYear();
   document.getElementById("year").textContent = currentYear;
 });
+
+// Bouton pour remonter en haut de la page
+window.onscroll = function () {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    document.getElementById("backToTop").style.display = "block";
+  } else {
+    document.getElementById("backToTop").style.display = "none";
+  }
+};
+
+document.getElementById("backToTop").onclick = function () {
+  document.body.scrollTop = 0; // Pour Safari
+  document.documentElement.scrollTop = 0; // Pour Chrome, Firefox, IE, Edge et Opera
+  document.body.focus(); // Pour éviter un bug de focus sur le bouton
+};
